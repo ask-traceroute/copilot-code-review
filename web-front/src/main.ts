bb33d256fb0d783e6ef32f48d6befa3cc6f7a5c5
sukeroku <ask.traceroute@gmail.com>
2025-07-20 12:29:53 +0900

    変更

--- web-front/src/main.ts
+++ web-front/src/main.ts
@@ -1,11 +1,15 @@
 import "./style.css"
 
-// カウンターの実装
-const counterButton = document.getElementById("counter");
-if (counterButton) {
-  let count = 0;
-  counterButton.addEventListener("click", () => {
-    count++;
-    counterButton.textContent = count.toString();
-  });
+function main() {
+  // カウンターの実装
+  const counterButton = document.getElementById("counter");
+  if (counterButton) {
+    let count = 0;
+    counterButton.addEventListener("click", () => {
+      count++;
+      counterButton.textContent = count.toString();
+    });
+  }
 }
+
+main();
